88e3b03ea06d077c5c3a829f914f929ef5c6be28
Hossam Amer <hossam.amer@multicom-mac2.uwaterloo.ca>
2019-09-12 14:03:04 -0400

    Writing code for using ffmpeg for HEVC

---
+++ calc_frame_size.sh
@@ -0,0 +1,12 @@
+f="./test_file/ILSVRC2012_val_00001000_504_336_RGB_0_1.265"
+
+# frame_size=$(ffprobe -show_frames $f | grep pkt_size=)
+# frame_size=$(echo $frame_size | tr -cd [:digit:])
+# echo 'frame_size:' $frame_size
+
+frame_size=$(./hevcesbrowser_console -i $f)
+./hevcesbrowser_console -i $f >> go.txt
+frame_size=$(grep '^0x*' go.txt) # lines start with
+
+echo $frame_size
+
---
+++ jpeg_hevc_transcode.sh
@@ -0,0 +1,9 @@
+INPUT_FILE="./test_file/ILSVRC2012_val_00001000_504_336_RGB.yuv"
+OUTPUT_ENC_FILE="./test_file/1000_0.265"
+OUTPUT_DEC_FILE="./test_file/1000_0.yuv"
+QP=0
+# ffmpeg -i $INPUT_FILE -c:v libx265 -crf 0 output.mp4
+
+# ffmpeg -f rawvideo -vcodec rawvideo -s 504x336 -pix_fmt yuv420p -i $INPUT_FILE -c:v libx265 -crf $QP -preset medium -x265-params profile=main:level-idc=50:high-tier:vbv-bufsize=100000:vbv-maxrate=100000 $OUTPUT_DEC_FILE
+ffmpeg -f rawvideo -vcodec rawvideo -s 504x336 -pix_fmt yuv420p -i $INPUT_FILE -c:v libx265 -crf $QP -preset medium $OUTPUT_DEC_FILE
+
