84260689b280bcb058c86973b57efc9d2fd265f8
Hossam Amer <h2amer@multicom-pc52.eng.uwaterloo.ca>
2019-09-13 12:41:28 -0400

    cleanup file

--- cleanup.sh
+++ cleanup.sh
@@ -1,3 +1,3 @@
 
 rm /media/h2amer/MULTICOM102/103_HA/MULTICOM103/set_yuv/Seq-RECONS-ffmpeg/1/*; rm /media/h2amer/MULTICOM102/103_HA/MULTICOM103/set_yuv/Seq-265-ffmpeg/1/*
-rm /media/h2amer/MULTICOM102/103_HA/MULTICOM103/set_yuv/Gen/Seq-Stats/1/*
+rm /media/h2amer/MULTICOM102/103_HA/MULTICOM103/set_yuv/Gen/Seq-Stats/*
